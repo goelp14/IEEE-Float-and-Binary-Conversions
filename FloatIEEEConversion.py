3f813adf3f2405e8366bc43c00358f99ae891d15
Pranav Goel <pranavgoel11@hotmail.com>
2019-02-04 20:56:46 -0600

    Created Python Conversion Script Final and updated ReadME

---
+++ FloatIEEEConversion.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+# include standard modules
+import argparse
+import sys
+
+__author__ = "Pranav Goel"
+__copyright__ = "Copyright 2019"
+__credits__ = ["Pranav Goel"]
+__license__ = "Apache 2.0"
+__version__ = "1.0.1"
+__maintainer__ = "Pranav Goel"
+__email__ = "pranavg4@illinois.edu"
+__status__ = "Prototype"
+
+def main():
+  print("This is a program that helps convert to and from IEEE 754 floating point with binary.\n\nWhat makes this program different from others, is that it shows the work done as well.\n\nThis should make it easier for various homeworks where you have to show all the work.\n\nI hope this makes your life easier!\n\n-Pranav Goel\n\n")
+
+  text = "This is a program that helps convert to and from IEEE 754 floating point with binary.\n\nWhat makes this program different from others, is that it shows the work done as well.\n\nThis should make it easier for various homeworks where you have to show all the work.\n\nI hope this makes your life easier!\n\n-Pranav Goel\n\n------------------------------------------------------------------------------\n\nUsage tips:\nWhen putting in a binary string that has spaces in it, make sure you put \"\" around it.\n\nFor example: 0100 0000 0100 1001 0000 1110 0101 0110 should be written as \"0100 0000 0100 1001 0000 1110 0101 0110\" instead.\n\nThis is because the program would otherwise read the string as multiple arguments."
+
+  CLIUsed = False
+
+  # initiate the parser
+  parser = argparse.ArgumentParser(description = text)
+
+  # add long and short argument
+  parser.add_argument("--floatToBinary", "-ftb", help="Convert a Float to Binary")
+  parser.add_argument("--binaryToFloat", "-btf", help="Convert a Binary String to a Float")
+
+  # read arguments from the command line
+  args = parser.parse_args()
+
+  if args.floatToBinary:
+    checkValid(True, args.floatToBinary)
+    print("Converting %s from a Float to a Binary" % args.floatToBinary)
+    floatToBinary(args.floatToBinary)
+    CLIUsed = True
+  if args.binaryToFloat:
+    y = "".join(str(args.binaryToFloat).split(" "))
+    print("Making sure there are no spaces:\n\n%s\n\n\n\n\n\n\n\n\n\n\n\nResult:\n\n" % y)
+    checkValid(False, y)
+    print("Converting %s from a Binary to a Float" % y[:32])
+    binaryToFloat(y)
+    CLIUsed = True
+
+  if(not CLIUsed):
+    choice = str(input("To convert from Binary to Float type \"0\".\n\nTo convert from Float to Binary type \"1\"\n\n"))
+    if (choice == "0"):
+      print("You are converting from Binary to Float")
+      binary = "".join(str(input("Type your Binary String:\n")).split(" "))
+      print("Making sure there are no spaces:\n\n%s\n\n\n\n\n\n\n\n\n\nResult:\n\n" % binary)
+      checkValid(False, binary)
+      binaryToFloat(binary)
+    elif(choice == "1"):
+      print("You are converting from Float to Binary")
+      f = str(input("Type in your float:\n"))
+      checkValid(True,f)
+      zero = checkZero(f)
+      if(zero[0]):
+        if(zero[1]):
+          print("Sign will be 1 which = - as it is -0. Since it is zero there is no need to do work as the result will always be 0. So your answer is:\n\n1 00000000 00000000000000000000000")
+        else:
+          print("Sign will be 0 which = + as it is 0. Since it is zero there is no need to do work as the result will always be 0. So your answer is:\n\n0 00000000 00000000000000000000000")
+      else:
+        floatToBinary(f)
+    else:
+      sys.exit("You did not choose a valid option. Please try again.")
+def checkZero(n):
+  checks = [False,False]
+  if(float(n) == 0):
+    checks[0] = True
+    if("-" in n):
+      checks[1] = True
+  return checks
+def checkValid(ftb, string):
+  s = string
+  valid = True
+  if(ftb):
+    try:
+      float(s)
+    except ValueError:
+      valid = False
+  else:
+    bCheck = set(s)
+    b = {'0', '1'}
+    if b == bCheck or bCheck == {'0'} or bCheck == {'1'}:
+      valid = True
+    else:
+      valid = False
+    if(len(string)<32):
+      sys.exit("Value is less than 32 bits, please try again")
+    elif(len(string)>32):
+      s = s[:32]
+      print("Cutting out any bits after 32 bits so that string is 32 bits.\nYour new string is %s" % s)
+  if(not valid):
+    sys.exit("Value not a binary string or a float please try again")
+
+def floatToBinary(num):
+  test = float(num)
+  if(int(test) == test and isinstance(test, float)):
+    floatNum = float(num)
+  else:
+    if("0." not in num):
+      floatNum = "0" + num
+    else:
+      floatNum = num
+  floatString = (str)(floatNum)
+  print(floatString)
+  normNum = ""
+  arrayFloat = floatString.split('.')
+  sign = "+"
+  for x in arrayFloat[0]:
+    if (x == '-'):
+      sign = "-"
+    else:
+      normNum += x
+  binNormNum = bin(int(normNum))[2:]
+  print("Sign: %s" % sign)
+  print("Before decimal point: %s" % normNum)
+  print("Before decimal point as binary: %s" % binNormNum)
+  res = mantissa(str(arrayFloat[1]))
+  print("Value after decimal point: %s" % res)
+  decibin = str(binNormNum) + "." + res
+  print("Results in: %s" % decibin)
+  normalized = normalize(str(binNormNum),str(res))
+  print("Normalized Value: %s\nPower: %s\nFinal Exponent: %s\nMantissa: %s" % (normalized[0],normalized[1],normalized[2],normalized[3]))
+  exponentBin = str(bin(int(normalized[2])))[2:]
+  if (len(exponentBin) < 8):
+    exponentBin = "0"*(8-len(exponentBin)) + str(bin(int(normalized[2])))[2:]
+
+  print("Exponent in Binary: %s" % exponentBin)
+  numZeros = 23 - len(normalized[3])
+  zeroString = ""
+  for y in range(0,numZeros):
+    zeroString += "0"
+  if (sign == "+"):
+    answer = "0" + exponentBin + normalized[3] + zeroString
+  else:
+    answer = "1" + exponentBin + normalized[3] + zeroString
+  print("The answer is: %s %s %s" % (answer[:-31],answer[1:9],answer[9:32]))
+
+def mantissa(numCon):
+  stop = 1
+  num = float("." + numCon)
+  zoro = ""
+  valstring = ""
+  val = num*2
+  count = 0
+  countOn = False
+  if(num == 0):
+    print("Since the value after the decimal is 0, there is no need to do any work as it will always be 0.")
+    zoro = "0"*25
+  else:
+    while ((val != stop) and (count != 27)):
+      if("1" in zoro):
+          countOn = True
+      if(countOn):
+          count += 1
+      if(val > 1):
+        zoro += "1"
+        valstring = str(val).split(".")
+        valNext = float("." + valstring[1])*2
+      else:
+        zoro += "0"
+        valNext = val *2
+      print("Work:\n%s * 2 = %s\nBuilding Binary: %s" % (val/2,val,zoro)
+      )
+      val = valNext
+      #print("Building Binary: %s" % zoro)
+    zoro += "1"
+    if(len(zoro) < 25):
+      print("0.5 * 2 = 1\nBuilding Binary: %s" % (zoro))
+  return(zoro)
+
+def normalize(binStart, binEnd):
+  start = binStart
+  end = binEnd
+  count = 0
+  startLen = len(binStart)
+  endLen = len(binEnd)
+  normalize = "1"
+  normalize += "."
+  if(start != "0"):
+    power = startLen - 1
+    normalize += start[1:]
+    normalize += end
+  else:
+    for x in end:
+      if (x == "0"):
+        count += 1
+      else:
+        break
+    normalize += end[(count+1):]
+    power = (count + 1) * -1
+  mantissaA = normalize.split(".")
+  mantissa = mantissaA[1][:24]
+  ct = 22
+  if(mantissa[23] == "1"):
+      one = True
+  else:
+      one = False
+  mantissa = mantissa[:23]
+  print(one)
+  for x in reversed(mantissa):
+      if(one):
+          if(x == "1"):
+              mantissa = mantissa[:ct] + mantissa[ct:ct+1].replace("1","0")+ mantissa[ct+1:]
+              one = True
+          else:
+              mantissa = mantissa[:ct] + mantissa[ct:ct+1].replace("0","1") + mantissa[ct+1:]
+              one = False
+              break
+          ct = ct - 1
+      else:
+        break
+  exponent = power + 127
+  result = [normalize,power,exponent,mantissa[:23]]
+  return result
+
+def binaryToFloat(binary):
+  binaryString = binary[:32]
+  if (len(binaryString) < 32):
+    print("You didn't put a 32 bit string")
+  else:
+    sign,exponent,mantissa = binaryString[:1], binaryString[1:9],binaryString[9:32]
+  exponentFinal = int(exponent,2) - 127
+  print("Exponent is %s in Binary\nIn Decimal it is %s\nTo get to final exponent do %s - 127 = %s.\nThe exponent is %s" % (exponent,int(exponent,2),int(exponent,2),exponentFinal,exponentFinal))
+  work = ["0"]*23
+  workM = 0
+  finalWork = ""
+  mantissaL = list(mantissa)
+  for x in range(0,len(mantissa)):
+    work[x] = "(" + mantissaL[x] + " x 2^" + str((x+1)*-1) +")"
+    workM += int(mantissaL[x]) * (2**((x+1)*-1))
+  finalWork = " + ".join(work)
+  print ("In binary, the mantissa is: %s\n\nConverting to decimal:\n%s = %s\n\nSo the mantissa is: %s" % (mantissa,finalWork,workM,workM))
+  if(sign == "0"):
+    signChar = "+"
+  else:
+    signChar = "-"
+  print("\nThe sign is %s => %s" % (sign,signChar))
+  print("\nIn the formula you have:\n\n((-1)^%s) x 1.%s x (2^%s)" % (sign,str(workM)[2:],exponentFinal))
+  center = float("1." + str(workM)[2:])
+  FinalResult = ((-1)**int(sign))*center*(2**int(exponentFinal))
+  print("\nThe answer is: %s" % FinalResult)
+
+main()
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # IEEE-Float-and-Binary-Conversions
+
+_This program is designed to not only convert from the IEEE Float 754 standard to a Binary String and vice versa, but also display all the work along with it. The intent is to speed up the process when trying to complete these conversions and the work must be shown. I hope this can make lives easier in some way or another_
+
+##Usage
+You can access the features of this program by adding arguments when you run it from terminal or by simply running the program and following the built in steps.
+
+###Float to Binary
+####Terminal
+To use the Terminal follow this format:
+```bash
+python <path/to/script> -ftb <insert float here>
+```
+####From Program
+The Program will prompt you to type either a "0" or a "1" depending on if you want to convert from float to binary or binary to float. Simply type in the number corresponding to the option you want and follow the displayed instructions.
+
+###Binary to Float
+####Terminal
+To use the Terminal follow this format:
+```bash
+python <path/to/script> -btf <insert binary string here>
+```
+Note that you must place the binary string in "" if there are spaces in your binary string as the code would read them as seperate arguments. This way it is read as one string which is then converted into a binary string with no spaces.
+
+####From Program
+Same as with Float to Binary.
+
+##Extra Info
+
+I have many safeties put in place so if the program closes with an exit message, do not be alarmed. You likely put something in that the code can't work with. Simply fix that and run the code again.
+
+To see the help manual simply run the program but with the option **-h**.
+
+If you find any bugs please notify me.
+
+
